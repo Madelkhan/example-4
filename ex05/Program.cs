0da0cfc13d581f9c2dd860614d052bb8d745e886
Boss <madelkhan@gmail.com>
2022-05-25 23:05:52 +0600

    .\ex05\

---
+++ ex05/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 2;
+int b = 8;
+int c = 6;
+int d = 4;
+int e = 3;
+
+int max = a;
+
+if (a > max) max = a;
+
+if (b > max) max = b;
+
+if (c > max) max = c;
+
+if (d > max) max = d;
+
+if (e > max) max = e;
+
+Console.WriteLine(max);
