5ed922a3d43cd4bed69f5eba8f0c70fc4e0a0177
Boss <madelkhan@gmail.com>
2022-05-26 17:58:19 +0600

    .\ex07\

---
+++ ex07/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0;
+int distance = 10000;
+int firstfriendspeed = 1;
+int secondfriendspeed = 2;
+int dogspeed = 5;
+int friend = 2;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstfriendspeed + secondfriendspeed);
+        friend = 2;
+    }
+    else 
+    {
+        time = distance / (secondfriendspeed + dogspeed);
+        friend = 2;
+    }
+    distance = distance - (firstfriendspeed + secondfriendspeed) * time;
+    count ++;
+    distance ++;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count );
+Console.WriteLine(" раз");
